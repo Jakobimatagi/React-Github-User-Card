0e0d8596268a5c7bc8e3b6e655c4bd58095848a6
Jakobi Matagi <jkmatagi@hotmail.com>
2021-02-14 16:30:27 -0700

    mvp done

--- github-user-jakobi/src/App.js
+++ github-user-jakobi/src/App.js
@@ -1,8 +1,8 @@
-import './App.css';
-import React from 'react';
-import axios from 'axios';
-import Followers from './follower'
-
+import "./App.css";
+import React from "react";
+import axios from "axios";
+import Followers from "./follower";
+import styled from "styled-components";
 class App extends React.Component {
   constructor() {
     super();
@@ -10,50 +10,68 @@ class App extends React.Component {
       name: [],
       location: [],
       bio: [],
-
-    }
+    };
   }
 
-  componentDidMount(){
-    console.log('mounted');
-   this.githubData(this.state.username)
+  componentDidMount() {
+    console.log("mounted");
+    this.githubData(this.state.username);
   }
 
-  
-  githubData = getData => {
-    console.log("Get Data")
-    
+  githubData = (getData) => {
+    console.log("Get Data");
+
     axios
-  .get('https://api.github.com/users/jakobimatagi')
-  .then (res => { 
-    console.log(res.data)
-     this.setState({
-       ...this.state,
-       name: res.data.name,
-       location: res.data.location,
-       bio: res.data.bio,
-      
-     }) 
-  })
-  .catch(err => console.log(err))
-  }
+      .get("https://api.github.com/users/jakobimatagi")
+      .then((res) => {
+        console.log(res.data);
+        this.setState({
+          ...this.state,
+          name: res.data.name,
+          location: res.data.location,
+          bio: res.data.bio,
+        });
+      })
+      .catch((err) => console.log(err));
+  };
 
-  
-  
-
-
-  render (){
-
-    return(
-      <div className="App">
-          <h1>{this.state.name}</h1>
-          <h2>{this.state.bio}</h2>
-          <h3>{`Location: ${this.state.location}`}</h3>
+  render() {
+    return (
+      <Styleddiv>
+        <div className="App">
+          <div className="userCard">
+            <h1>{this.state.name}</h1>
+            <h2>Bio: {this.state.bio}</h2>
+            <h3>{`Location: ${this.state.location}`}</h3>
+          </div>
           <Followers />
-      </div>
-
-    )
+        </div>
+      </Styleddiv>
+    );
   }
 }
 
 export default App;
+
+const Styleddiv = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 15% 25%;
+  background-color: #5e548e;
+  color: white;
+  padding: 1rem 1rem;
+  border-radius: 15%;
+  
+
+  h1 {
+    background-color: #9f86c0;
+    padding: 1rem 1rem;
+    border-radius: 50%;
+  }
+  .userCard{
+    background-color: #e0b1cb;
+    padding: 1rem 1rem;
+    border-radius: 15%;
+  }
+`;
